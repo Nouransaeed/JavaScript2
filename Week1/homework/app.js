e34d7ab106f5f0f5e5b452ed8e9d43f907ebd284
A T <toegelanna@outlook.com>
2019-06-09 14:41:51 +0200

    classrum excercise 09 06 2019

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -1,11 +1,28 @@
 'use strict';
 
 {
-  const bookTitles = [
+  const favoriteGames = [
     // Replace with your own book titles
     'harry_potter_chamber_secrets',
+    'call_of_the_wild',
+    'the_turtle',
+    'snowball_and_the_bunny_adventures',
+    'chicken_little',
   ];
 
   // Replace with your own code
-  console.log(bookTitles);
+  console.log(favoriteGames);
+ // document.body.onload = addElement;
+ // function displayGameTitles = () => { 
+   
+    let unorderedList = document.createElement("ul"); 
+    for (const gameIndex in favoriteGames) { 
+      const listItem = document.createElement("li"); 
+      listItem.textContent = favoriteGames[gameIndex];
+      unorderedList.appendChild(listItem); 
+  }
+    // add the newly created element and its content into the DOM 
+    const currentDiv = document.getElementById("gameDiv"); 
+    document.body.insertBefore(unorderedList, currentDiv); 
+  
 }
