791c151cc4b9fb3f6858019de71e2555ee9ed60f
jshortz <jessica.shortz@gmail.com>
2019-07-04 20:04:32 +0200

    fixing branch issue in second file

--- Week2/homework/map-filter.js
+++ Week2/homework/map-filter.js
@@ -1,11 +1,17 @@
 'use strict';
 
-function doubleOddNumbers(numbers) {
-  // Replace this comment and the next line with your code
-  console.log(numbers);
-}
-
 const myNumbers = [1, 2, 3, 4];
+
+const doubleOddNumbers = numbers => {
+  const odds = numbers.filter(function(number) {
+    return number % 2 !== 0;
+  });
+  const doubles = odds.map(function(number) {
+    return number * 2;
+  });
+  return doubles;
+};
+
 console.log(doubleOddNumbers(myNumbers));
 
 // Do not change or remove anything below this line
