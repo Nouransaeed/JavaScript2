d2d2354a33ad58d883e983381093e567c2a2b8c2
A T <toegelanna@outlook.com>
2019-07-26 13:05:22 +0200

    2-1 for week3

--- Week3/homework/MY-step2-1.js
+++ Week3/homework/MY-step2-1.js
@@ -1,15 +1,22 @@
 'use strict';
+/*sample*/
+// function doIt() {
+//   console.log('I am done');
+// }
+// setTimeout(doIt, 5000);
 
+//Test by copy-pasting this function to the browser console
 function foo(func) {
   // What to do here?
   // Replace this comment and the next line with your code
+  return ('Hi, I am AaaaT, who are you?');
   console.log(func);
 }
 
 function bar() {
   console.log('Hello, I am bar!');
 }
-
+setTimeout(bar, 3000);
 foo(bar);
 
 // Do not change or remove anything below this line
