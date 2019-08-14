bb1baa6f6ed41ab70d97dc9e4d7d9afd36ee85a8
Nour Saeed <noursaeed8@hotmail.com>
2019-08-14 22:34:22 +0200

    Co-authored-by: Gabe Rodriguez <grod220@gmail.com>

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -1,50 +1,31 @@
-
 'use strict';
-{
 
-	const title = [
+const titles = [
+  'harry_potter_chamber_secrets',
+  'alchemist',  //Paulo coelho
+  'paula',
+  'orlando',
+  'divine-comedy',
+  'the-odyssey',
+];
 
-		 'harry_potter_chamber_secrets',
-
-    'alchemist',  //Paulo coelho
-    'paula',
-    'orlando',
-    'divine-comedy',
-    'the-odyssey',
-	];
-
-	let objBooks =  {
-
-        "harry_potter_chamber_secrets" :
-        { 
-       
-        title :"harry_potter_chamber_secrets",
-      
-        language:"english",
-    
-        author:"Joanne K. Rowling",
-    
-      },
-
-      "alchemist":
-      {
-        title:"alchemist",
-    
-        language:"english",
-    
-        author:"paulo-coelho",
-    
-      },
-    
-    "paula":{
-    
-        title:"paula",
-    
-        language:"english",
-    
-        author:"isabel allende",
-    
-      },
+let objBooks = {
+  "harry_potter_chamber_secrets": {
+    title :"harry_potter_chamber_secrets",
+    language:"english",
+    author:"Joanne K. Rowling"
+  },
+  "alchemist": {
+    title:"alchemist",
+    language:"english",
+    author:"paulo-coelho",
+  },
+  
+    "paula": { 
+      title:"paula", 
+      language:"english",
+      author:"isabel allende",
+    },
     "orlando":
       {
         title:"orlando",
@@ -75,37 +56,34 @@
         author:"homeros",
     
       }
-    
-    
-
   }
 
+  const ul = document.createElement('ul');
+
+  ul.setAttribute('id', 'booktitle');
 
 
-	function listBooks() {
 
-		let i = 0;
 
-		let ul = document.createElement('ul')
-
-		for (i = 0; i < title.length; i++) {
-
-			let li = document.createElement('li');
-
-			li.textContent = title[i];
-
-			ul.appendChild(li);
-    }
+/*  function listBooks() {
+  // let i = 0; // this is unnecessary
+  let ul = document.createElement('ul')
+  for (let i = 0; i < title.length; i++) { 
+    let li = document.createElement('li');
+    li.textContent = title[i];
+    ul.appendChild(li);
   }
+  // we don't do anything with the ul
+  // ???
+}
 
-		document.body.appendChild(ul);
+		document.body.appendChild(ul); // where does ul come from? This will throw an error. 
 	  document.body.onload = listBooks;
     const objBooksArr = [];
     for (const newListBooks in objBooks) {
     console.log(newListBooks);
     objBooksArr.push(objBooks[newListBooks])
-
-}
+    }
 
 
 
--- Week3/homework/step2-3.js
+++ Week3/homework/step2-3.js
@@ -3,35 +3,38 @@
 function repeatStringNumTimesWithFor(str, num) {
   let result = '';
   for (let i = 0; i < num; i++) {
-  result += str;
+    result += str;
   }
   return result;
-console.log('for', repeatStringNumTimesWithFor('abc', 3)),
+}
+
+console.log('for', repeatStringNumTimesWithFor('abc', 3))
+
 //while loop 
 function repeatStringNumTimesWithWhile(str, num) {
-let result = ''
-let counter = 0;
-while (counter < num) {
-result += str;
-counter++;
+  let result = ''
+  let counter = 0;
+  while (counter < num) {
+    result += str;
+    counter++;
   }
-return result;
+  return result;
 }
 console.log('while', repeatStringNumTimesWithWhile('abc', 3));
 function repeatStringNumTimesWithDoWhile(str, num) {
   let result = '';
   let i = 0;
   if (num > 0) {
-  do {
-  result += str,
+    do {
+      result += str;
       i++;
     } while (i < num);
   }
   return result;
 }
-console.log('do-while', repeatStringNumTimesWithDoWhile('abc', 0)),
+console.log('do-while', repeatStringNumTimesWithDoWhile('abc', 0));
 module.exports = {
-repeatStringNumTimesWithFor,
-repeatStringNumTimesWithWhile,
-repeatStringNumTimesWithDoWhile,
+  repeatStringNumTimesWithFor,
+  repeatStringNumTimesWithWhile,
+  repeatStringNumTimesWithDoWhile,
 };
