963fdde54e9ee08518b12ecfdddcc1ccb2722400
Nour Saeed <noursaeed8@hotmail.com>
2019-08-14 15:39:36 +0200

    homework6

--- Week3/homework/step2-6.js
+++ Week3/homework/step2-6.js
@@ -1,23 +1,48 @@
 'use strict';
-
+{
 const arr2d = [[1, 2], [3, 4], [5, 6]];
+
 const arr3d = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
+}
 
 function flattenArray2d(arr) {
-  // Replace this comment and the next line with your code
-  console.log(arr);
+
+  let flattenArr2 = [];
+
+  for (let i = 0; i < arr.length; i++) {
+
+    for (let j = 0; j < arr[i].length; j++) {
+
+      flattenArr2.push(arr[i][j])
+
+    }
+  }
+  return flattenArray2d;
 }
 
 function flattenArray3d(arr) {
-  // Replace this comment and the next line with your code
-  console.log(arr);
+
+  const flattenArr3 = [];
+
+  for (let i = 0; i < arr.length; i++) {
+
+    for (let j = 0; j < arr[i].length; j++) {
+
+      for (let l = 0; l < arr[i][j].length; l++) {
+
+        flattenArr3.push(arr[i][j][l]);
+
+      }}}
+
+  return flattenArray3d;
+
 }
 
-console.log(flattenArray2d(arr2d)); // -> [1, 2, 3, 4, 5, 6]
-console.log(flattenArray3d(arr3d)); // -> [1, 2, 3, 4, 5, 6, 7, 8]
 
-// Do not change or remove anything below this line
 module.exports = {
+
   flattenArray2d,
+
   flattenArray3d,
+
 };
