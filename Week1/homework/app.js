5e575a9cd8af656f6ad88e750c8383048523881b
Nour Saeed <noursaeed8@hotmail.com>
2019-08-15 18:19:30 +0200

    correct

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -1,100 +1,65 @@
-'use strict';
 
-const titles = [
-  'harry_potter_chamber_secrets',
-  'alchemist',  //Paulo coelho
-  'paula',
-  'orlando',
-  'divine-comedy',
-  'the-odyssey',
-];
+'use strict'
+const books = [
+  {
+    title :"Harry Potter Chamber of Secrets",
+    language:"english",
+    author:"Joanne K. Rowling",
 
-let objBooks = {
-  "harry_potter_chamber_secrets": {
-    title :"harry_potter_chamber_secrets",
-    language:"english",
-    author:"Joanne K. Rowling"
-  },
-  "alchemist": {
-    title:"alchemist",
-    language:"english",
-    author:"paulo-coelho",
   },
   
-    "paula": { 
-      title:"paula", 
-      language:"english",
-      author:"isabel allende",
-    },
-    "orlando":
-      {
-        title:"orlando",
-    
-        language:"english",
-    
-        author:"virgina wolf",
-    
-      },
-    "divine-comedy" :
-       {
-    
-        title:"divine-comedy",
-    
-        language:"English",
-    
-        author:"dante",
-    
-      },
-    
-      "the-odyssey":
-      {
-    
-        title:"the-odyssey",
-    
-        language: "english",
-    
-        author:"homeros",
-    
-      }
-  }
+  {
+    title:"Alchemist",
+    language:"english",
+    author:"paulo-co",
 
-  const ul = document.createElement('ul');
+  },
 
-  ul.setAttribute('id', 'booktitle');
+  { 
+   title:"Paula", 
+   language:"english",
+   author:"isabel allende",
 
+  },
 
+  {
 
+    title:"Orlando",
+    language:"english",
+    author:"virginia-wolf",
 
-/*  function listBooks() {
-  // let i = 0; // this is unnecessary
-  let ul = document.createElement('ul')
-  for (let i = 0; i < title.length; i++) { 
-    let li = document.createElement('li');
-    li.textContent = title[i];
-    ul.appendChild(li);
-  }
-  // we don't do anything with the ul
-  // ???
+  },
+
+  {
+     title:"Divine Comedy",
+     language:"English",
+     author:"dante",
+
+  },
+
+  {
+    title:"The Odyssey",
+    language: "english",
+    author:"Homeros",
+
+  },
+
+]
+
+function createAndAppend(parent,typ,attributes={}) {
+
+	const elem = document.createElement(typ)
+
+	parent.appendChild(elem)
+
+	for (const key in attributes)
+
+    elem[key] = attributes[key]	
+     return elem
 }
 
-		document.body.appendChild(ul); // where does ul come from? This will throw an error. 
-	  document.body.onload = listBooks;
-    const objBooksArr = [];
-    for (const newListBooks in objBooks) {
-    console.log(newListBooks);
-    objBooksArr.push(objBooks[newListBooks])
-    }
-
-
-
-const bookPic = {
-
-    'harry_potter_chamber_secrets':potter.jpg ,
-    'alchemist':alchemist,  
-    'paula' :paula.jpg,
-    'orlando':woolf.jpg,
-    'divine-comedy':divine.jpg,
-    'the-odyssey' :odyssy.jpg,
-};
-
-
+const ul = createAndAppend(document.body, 'ul', {})
+for (const index in books) {
+	const book = books[index]
+	createAndAppend(ul, 'li', {value : index, innerHTML: book.title})
+}
